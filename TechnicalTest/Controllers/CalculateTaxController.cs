466d41cfa3dbccae7d5a5ca9f521c935de4de592
bf <bf@DESKTOP-85MOEML>
2023-11-05 13:59:36 +0530

    Add project files.

---
+++ TechnicalTest/Controllers/CalculateTaxController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TechnicalTest.Controllers
+{
+    public class CalculateTaxController : ApiController
+    {
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
---
+++ TechnicalTest/Models/ReturnModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechnicalTest.Models
+{
+    public class ReturnModel
+    {
+        /// <summary>
+        /// SaveStatus
+        /// </summary>
+        public bool SaveStatus { get;set;}
+    }
+}
