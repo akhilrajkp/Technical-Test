e590cf31277ac50cb214d877b9f16819140373bb
AkhilRaj KP <akhilraj.kp@gmail.com>
2023-11-07 13:51:18 +0530

    Technical Test Commit

--- TechnicalTest/Controllers/CalculateTaxController.cs
+++ TechnicalTest/Controllers/CalculateTaxController.cs
@@ -4,15 +4,54 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml;
+using TechnicalTest.Models;
 
 namespace TechnicalTest.Controllers
 {
     public class CalculateTaxController : ApiController
     {
-        public string Get(int id)
+        #region CalculateSalesTax
+        /// <summary>
+        /// CalculateSalesTax
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public HttpResponseMessage CalculateSalesTax(string requestMessage)
         {
-            return "value";
-        }
+            try
+            {
+                decimal total = 0;
+                decimal salesTax = 0;
+                decimal grandTotal = 0;
+                int salesTaxPercentage = 18;
+                string costCentre = "";
 
+                XmlDocument xmltest = new XmlDocument();
+                xmltest.LoadXml("<body>" + requestMessage + "</body>");
+                XmlNodeList nodetotal = xmltest.GetElementsByTagName("total");
+                XmlNodeList nodecostcentre = xmltest.GetElementsByTagName("cost_centre");
+                total = nodetotal.Count > 0 ? Convert.ToDecimal(nodetotal[0].InnerText) : 0;
+                costCentre = nodecostcentre.Count > 0 ? nodecostcentre[0].InnerText : "UNKNOWN";
+
+                if (total > 0)
+                {
+                    salesTax = (total * salesTaxPercentage) / 100;
+                    grandTotal = total - salesTax;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Rejected");
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new ReturnModel { Total = total, SalesTax = salesTax, GrandTotal = grandTotal, CostCentre = nodecostcentre.Count > 0 ? costCentre : "UNKNOWN" });
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Rejected");
+            }
+        }
+        #endregion
     }
 }
--- TechnicalTest/Models/ReturnModel.cs
+++ TechnicalTest/Models/ReturnModel.cs
@@ -7,9 +7,32 @@ namespace TechnicalTest.Models
 {
     public class ReturnModel
     {
+        #region Total
         /// <summary>
-        /// SaveStatus
+        /// Total
         /// </summary>
-        public bool SaveStatus { get;set;}
+        public decimal Total { get; set; }
+        #endregion
+
+        #region SalesTax
+        /// <summary>
+        /// SalesTax
+        /// </summary>
+        public decimal SalesTax { get; set; }
+        #endregion
+
+        #region GrandTotal
+        /// <summary>
+        /// GrandTotal
+        /// </summary>
+        public decimal GrandTotal { get; set; }
+        #endregion
+
+        #region CostCentre
+        /// <summary>
+        /// CostCentre
+        /// </summary>
+        public string CostCentre { get; set; }
+        #endregion               
     }
 }
